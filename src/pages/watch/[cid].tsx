6955c350f9e51b32b3fb205592f72f4eabc1ae0e
anarkrypto <anarkrypto@gmail.com>
2022-03-28 12:48:05 -0300

    add favicon

--- src/pages/watch/[cid].tsx
+++ src/pages/watch/[cid].tsx
@@ -19,7 +19,7 @@ export default function Watch() {
     return (
         <>
             <Head>
-                <title>| SeaTube Watch</title>
+                <title>SeaTube Watch</title>
                 <meta name='viewport' content='initial-scale=1.0, width=device-width' />
             </Head>
             <Layout>
